54860884ac6320038907a3cde433a30c96c67f53
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2021-11-08 14:22:45 +0100

    Implemented Sport enum methods

--- src/it/unibo/oop/lab/enum2/Sport.java
+++ src/it/unibo/oop/lab/enum2/Sport.java
@@ -16,7 +16,6 @@ package it.unibo.oop.lab.enum2;
  * 
  */
 public enum Sport {
-
     /*
      * TODO
      * 
@@ -37,6 +36,13 @@ public enum Sport {
      * - soccer
      * 
      */
+    BASKET(Place.INDOOR, 5, "Basket"),
+    VOLLEY(Place.INDOOR, 6, "Volley"),
+    TENNIS(Place.OUTDOOR, 1, "Tennis"),
+    BIKE(Place.OUTDOOR, 1, "Bike"),
+    F1(Place.OUTDOOR, 1, "F1"),
+    MOTOGP(Place.OUTDOOR, 1, "MotoGP"),
+    SOCCER(Place.OUTDOOR, 11, "Soccer");
 
     /*
      * TODO
@@ -45,6 +51,9 @@ public enum Sport {
      * 
      * Declare required fields
      */
+    private final int members;
+    private final String name;
+    private final Place inOut;
 
     /*
      * TODO
@@ -55,6 +64,11 @@ public enum Sport {
      * 
      * - Sport(final Place place, final int noTeamMembers, final String actualName)
      */
+    private Sport(final Place place, final int noTeamMembers, final String actualName) {
+	this.inOut = place;
+	this.members = noTeamMembers;
+	this.name = new String(actualName);
+    }
 
     /*
      * TODO
@@ -65,20 +79,35 @@ public enum Sport {
      * 1) public boolean isIndividualSport()
      * 
      * Must return true only if called on individual sports
-     * 
-     * 
+     */
+    public boolean isIndividualSport() {
+	return this.members == 1;
+    }
+
+    /*
      * 2) public boolean isIndoorSport()
      * 
      * Must return true in case the sport is practices indoor
-     * 
-     * 
+     */
+    public boolean isIndoorSport() {
+	return this.inOut == Place.INDOOR;
+    }
+
+    /*
      * 3) public Place getPlace()
      * 
      * Must return the place where this sport is practiced
-     * 
-     * 
+     */
+    public Place getPlace() {
+	return this.inOut;
+    }
+
+    /*
      * 4) public String toString()
      * 
      * Returns the string representation of a sport
      */
+    public String toString() {
+	return "[name: " + this.name + ", members: " + members + ", In/Out: " + inOut.toString();
+    }
 }
