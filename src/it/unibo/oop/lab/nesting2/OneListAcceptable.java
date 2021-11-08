4e9c20c40251dae1f31477eee32bbd13a74ca7c6
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2021-11-08 15:30:28 +0100

    Implemented OneListAcceptable and his nested Acceptor

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,48 @@
+package it.unibo.oop.lab.nesting2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+
+    class OneListAcceptor implements Acceptor<T> {
+
+	List<T> acceptorList;
+	int index;
+
+	OneListAcceptor(List<T> list) {
+	    acceptorList = new ArrayList<>(list);
+	    index = 0;
+	}
+
+	@Override
+	public void accept(T newElement) throws ElementNotAcceptedException {
+	    if (Integer.compare(index, acceptorList.size()) < 0 && acceptorList.get(index).equals(newElement)) {
+		index++;
+	    } else {
+		throw new ElementNotAcceptedException(newElement);
+	    }
+
+	}
+
+	@Override
+	public void end() throws EndNotAcceptedException {
+	    if (Integer.compare(index, acceptorList.size()) != 0) {
+		throw new EndNotAcceptedException();
+	    }
+
+	}
+    };
+
+    OneListAcceptor acc;
+
+    public OneListAcceptable(List<T> list) {
+	acc = new OneListAcceptor(list);
+    }
+
+    @Override
+    public Acceptor<T> acceptor() {
+	return acc;
+    }
+
+}
