41905680f2e0be66ba84fe3f96319f28355f14c2
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2021-11-08 14:44:55 +0100

    Implemented SportSocialNetworkUser with enums

--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -3,6 +3,9 @@
  */
 package it.unibo.oop.lab.enum2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
@@ -20,8 +23,7 @@ import it.unibo.oop.lab.socialnetwork.User;
  * 
  * 2) complete the implementation of the new methods as explained below
  * 
- * @param <U>
- *            specific {@link User} type
+ * @param <U> specific {@link User} type
  */
 public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUserImpl<U> {
 
@@ -30,37 +32,31 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
+    private final Set<Sport> sports = new HashSet<>();
 
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
+     * @param name    the user firstname
+     * @param surname the user lastname
+     * @param user    alias of the user, i.e. the way a user is identified on an
+     *                application
      */
     public Sport2SocialNetworkUserImpl(final String name, final String surname, final String user) {
-        this(name, surname, user, -1);
+	this(name, surname, user, -1);
     }
 
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param userAge
-     *            user's age
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
+     * @param name    the user firstname
+     * @param surname the user lastname
+     * @param userAge user's age
+     * @param user    alias of the user, i.e. the way a user is identified on an
+     *                application
      */
     public Sport2SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
+	super(name, surname, user, userAge);
     }
 
     /*
@@ -68,26 +64,24 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      */
 
     /**
-     * Add a new sport followed by this user: if the user already likes or does
-     * the sport, nothing happens.
+     * Add a new sport followed by this user: if the user already likes or does the
+     * sport, nothing happens.
      * 
-     * @param sport
-     *            a sport followed/done by the user
+     * @param sport a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+	sports.add(sport);
     }
 
     /**
      * Returns true if a user likes/does a given sport.
      * 
-     * @param s
-     *            sport to check
+     * @param s sport to check
      * 
      * @return true if user likes sport s
      */
     public boolean likesSport(final Sport s) {
-        return false;
+	return sports.contains(s);
     }
 
     /*
@@ -95,23 +89,33 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      */
 
     /**
-     * Returns the set of individual sports followed/practiced by this user: a
-     * sport is individual if the number of team member is = 1.
+     * Returns the set of individual sports followed/practiced by this user: a sport
+     * is individual if the number of team member is = 1.
      * 
      * @return the set of individual sport this user practices/follows
      */
-    /*
-     * public Set<Sport> getIndividualSports() { return null; }
+
+    public Set<Sport> getIndividualSports() {
+	return new HashSet<>(this.sports);
+    }
+
+    /**
+     * Returns the set of sports which are practiced in a given place.
      * 
-     * 
-     * /** Returns the set of sports which are practiced in a given place.
-     * 
-     * @param p the place in which the sport is practiced in order to be
-     * included in the resulting set
+     * @param p the place in which the sport is practiced in order to be included in
+     *          the resulting set
      * 
      * @return the set of sport practiced in a given place
      */
-    /*
-     * public Set<Sport> getSportPracticedInPlace(Place p) { return null; }
-     */
+
+    public Set<Sport> getSportPracticedInPlace(Place p) {
+	Set<Sport> result = new HashSet<>();
+	for (Sport s : this.sports) {
+	    if (s.getPlace() == p) {
+		result.add(s);
+	    }
+	}
+	return result;
+    }
+
 }
