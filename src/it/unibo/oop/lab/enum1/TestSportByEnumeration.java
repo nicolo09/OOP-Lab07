175cb8cd7ad2e7847fdd9e945f5ae11b865c60f9
Nicolò Guerra <nicolo.guerra@studio.unibo.it>
2021-11-19 10:57:23 +0100

    Reformatted enum1 tests

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -22,62 +22,58 @@ public final class TestSportByEnumeration {
      * @param args ignored
      */
     public static void main(final String... args) {
-	/*
-	 * [TEST DEFINITION]
-	 * 
-	 * By now, you know how to do it
-	 */
-	// TODO
+        /*
+         * [TEST DEFINITION]
+         * 
+         * By now, you know how to do it
+         */
+        // TODO
 
-	/*
-	 * Davide Cassani , dcassani, 53
-	 * 
-	 * Bernie Ecclestone, becclestone, 83
-	 * 
-	 * Fernando Alonso, falonso, 34
-	 */
-	final SportSocialNetworkUserImpl<User> dcassani = new SportSocialNetworkUserImpl<User>("Davide", "Cassani",
-		"dcassani");
-	final SportSocialNetworkUserImpl<User> becclestone = new SportSocialNetworkUserImpl<User>("Bernie",
-		"Ecclestone", "becclestone");
-	final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<User>("Fernando", "Alonso",
-		"falonso");
-	
-	/*
-	 * TODO: alonso is a fan of soccer and bike and practices F1: add these sports
-	 * to Alonso
-	 * 
-	 * falonso.addSport(SportSocialNetworkUserImpl.F1);
-	 * 
-	 * ...
-	 */
-	falonso.addSport(SportSocialNetworkUserImpl.SOCCER);
-	falonso.addSport(SportSocialNetworkUserImpl.F1);
+        /*
+         * Davide Cassani , dcassani, 53 // * Bernie Ecclestone, becclestone, 83
+         * 
+         * Fernando Alonso, falonso, 34
+         */
+        final SportSocialNetworkUserImpl<User> dcassani = new SportSocialNetworkUserImpl<User>("Davide", "Cassani",
+                "dcassani");
+        final SportSocialNetworkUserImpl<User> becclestone = new SportSocialNetworkUserImpl<User>("Bernie",
+                "Ecclestone", "becclestone");
+        final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<User>("Fernando", "Alonso",
+                "falonso");
 
-	System.out.println("Alonso practices F1: " + falonso.hasSport(SportSocialNetworkUserImpl.F1));
-	System.out.println("Alonso does not like volley: " + !falonso.hasSport(SportSocialNetworkUserImpl.VOLLEY));
-	
-	/*
-	 * Cassani practiced bike and loves F1 and MotoGP
-	 */
-	dcassani.addSport(SportSocialNetworkUserImpl.BIKE);
-	dcassani.addSport(SportSocialNetworkUserImpl.F1);
-	dcassani.addSport(SportSocialNetworkUserImpl.MOTOGP);
-	System.out.println(
-		"Cassani has been a professional biker: "
-		+ dcassani.hasSport(SportSocialNetworkUserImpl.BIKE)
-	);
-	System.out.println("Cassani does not like soccer: " + !dcassani.hasSport(SportSocialNetworkUserImpl.SOCCER));
-	
-	/**
-	 * Ecclestone is the de-facto owner of F1 but also a fan of basket.
-	 */
-	becclestone.addSport(SportSocialNetworkUserImpl.F1);
-	becclestone.addSport(SportSocialNetworkUserImpl.BASKET);
-	System.out.println(
-		"Bernie's the boss when it comes to F1: " + becclestone.hasSport(SportSocialNetworkUserImpl.F1));
-	System.out.println(
-		"Bernie does love playing also basket: " + becclestone.hasSport(SportSocialNetworkUserImpl.BASKET));
+        /*
+         * TODO: alonso is a fan of soccer and bike and practices F1: add these sports
+         * to Alonso
+         * 
+         * falonso.addSport(SportSocialNetworkUserImpl.F1);
+         * 
+         * ...
+         */
+        falonso.addSport(SportSocialNetworkUserImpl.SOCCER);
+        falonso.addSport(SportSocialNetworkUserImpl.F1);
+
+        System.out.println("Alonso practices F1: " + falonso.hasSport(SportSocialNetworkUserImpl.F1));
+        System.out.println("Alonso does not like volley: " + !falonso.hasSport(SportSocialNetworkUserImpl.VOLLEY));
+
+        /*
+         * Cassani practiced bike and loves F1 and MotoGP
+         */
+        dcassani.addSport(SportSocialNetworkUserImpl.BIKE);
+        dcassani.addSport(SportSocialNetworkUserImpl.F1);
+        dcassani.addSport(SportSocialNetworkUserImpl.MOTOGP);
+        System.out.println(
+                "Cassani has been a professional biker: " + dcassani.hasSport(SportSocialNetworkUserImpl.BIKE));
+        System.out.println("Cassani does not like soccer: " + !dcassani.hasSport(SportSocialNetworkUserImpl.SOCCER));
+
+        /**
+         * Ecclestone is the de-facto owner of F1 but also a fan of basket.
+         */
+        becclestone.addSport(SportSocialNetworkUserImpl.F1);
+        becclestone.addSport(SportSocialNetworkUserImpl.BASKET);
+        System.out.println(
+                "Bernie's the boss when it comes to F1: " + becclestone.hasSport(SportSocialNetworkUserImpl.F1));
+        System.out.println(
+                "Bernie does love playing also basket: " + becclestone.hasSport(SportSocialNetworkUserImpl.BASKET));
 
     }
 }
